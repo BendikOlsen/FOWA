a4dfe1fa6421c4cfb8729907e986bba490c56715
bendikolsen <bendikolsen@gmail.com>
2020-01-07 14:36:20 +0100

    1.9 unicafe step4 finished

--- Part 1/unicafe/src/index.js
+++ Part 1/unicafe/src/index.js
@@ -14,11 +14,23 @@ const Statistics = (props) => {
     const good = props.good
     const neutral = props.neutral
     const bad = props.bad
-
     const totalFeedback = good + neutral + bad
+
     const positive = (positive, total) => {
         return positive / total * 100
     }
+    
+    console.log(good)
+    console.log(props.feedbackGiven)
+    if(props.feedbackGiven === 0) {
+        return (
+            <div>
+                <h1>statistics</h1>
+                <p>No feedback given</p>
+            </div>
+        )
+    } 
+
     return (
         <div>
             <h1>statistics</h1>
@@ -30,6 +42,7 @@ const Statistics = (props) => {
             <p>positive {positive(good, totalFeedback)} %</p>
         </div>
     )
+    
 }
 
 
@@ -37,25 +50,28 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-
+    const [feedbackGiven, setAll] = useState(0)
+    
     const goodClick = () => {
+        setAll(feedbackGiven +1)
         setGood(good +1)
     }
     const neutralClick = () => {
+        setAll(feedbackGiven +1)
         setNeutral(neutral +1)
     }
     const badClick = () => {
+        setAll(feedbackGiven +1)
         setBad(bad +1)
     }
 
-   
     return (
         <div>
         <h1>give feedback</h1>
             <Button onClick={goodClick} text='good' />
             <Button onClick={neutralClick} text='neutral' />
             <Button onClick={badClick} text='bad' />
-            <Statistics  good={good} neutral={neutral} bad={bad} />
+            <Statistics  good={good} neutral={neutral} bad={bad} feedbackGiven={feedbackGiven} />
         </div>   
     )
 }
